1efb0526ae36ba49208b9488b1ef574d1b8c1fa6
Krystian Gaj <krystian0120@outlook.com>
2024-02-07 18:47:32 +0100

    Wrap responses in Result - WIP

--- app/src/main/java/com/example/techquiz/viewmodel/GivenAnswerViewModel.kt
+++ app/src/main/java/com/example/techquiz/viewmodel/GivenAnswerViewModel.kt
@@ -4,15 +4,26 @@ import androidx.lifecycle.ViewModel
 import com.example.techquiz.data.domain.GivenAnswer
 import com.example.techquiz.data.domain.QuizResult
 import com.example.techquiz.data.repository.GivenAnswerRepository
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 
 class GivenAnswerViewModel(
     private val givenAnswerRepository: GivenAnswerRepository,
 ) : ViewModel() {
+    private val _answerAddResult = MutableSharedFlow<Result<Unit>>()
+    val answerAddResult
+        get() = _answerAddResult.asSharedFlow()
+
     private val _quizResults = mutableListOf<QuizResult>()
     val quizResults get() = _quizResults.toList()
 
     suspend fun addAnswer(answer: GivenAnswer) {
-        givenAnswerRepository.insertAnswer(answer)
-        _quizResults.add(QuizResult(answer.question.text, answer.correct))
+        try {
+            givenAnswerRepository.insertAnswer(answer)
+            _quizResults.add(QuizResult(answer.question.text, answer.correct))
+            _answerAddResult.emit(Result.success(Unit))
+        } catch (ex: Exception) {
+            _answerAddResult.emit(Result.failure(ex))
+        }
     }
 }
