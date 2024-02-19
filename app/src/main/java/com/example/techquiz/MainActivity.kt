7eb379765fd2b570df18491cb603eef57683c689
Krystian Gaj <krystian0120@outlook.com>
2024-02-19 12:05:33 +0100

    Fix no opening third question issue
    Due to its former type (Result<Unit>), answerAddResult couldn't be distinguished after each answer.

--- app/src/main/java/com/example/techquiz/MainActivity.kt
+++ app/src/main/java/com/example/techquiz/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.techquiz
 
+// TODO use proper Material3 components
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
--- app/src/main/java/com/example/techquiz/viewmodel/GivenAnswerViewModel.kt
+++ app/src/main/java/com/example/techquiz/viewmodel/GivenAnswerViewModel.kt
@@ -10,7 +10,7 @@ import kotlinx.coroutines.flow.asStateFlow
 class GivenAnswerViewModel(
     private val givenAnswerRepository: GivenAnswerRepository,
 ) : ViewModel() {
-    private val _answerAddResult = MutableStateFlow<Result<Unit>?>(null)
+    private val _answerAddResult = MutableStateFlow<Result<Int>?>(null)
     val answerAddResult
         get() = _answerAddResult.asStateFlow()
 
@@ -21,7 +21,7 @@ class GivenAnswerViewModel(
         try {
             givenAnswerRepository.insertAnswer(answer)
             _quizResults.add(QuizResult(answer.question.text, answer.correct))
-            _answerAddResult.emit(Result.success(Unit))
+            _answerAddResult.emit(Result.success(answer.question.id))
         } catch (ex: Exception) {
             _answerAddResult.emit(Result.failure(ex))
         }
