2e94f6c53ed8973ac6c492f33942d20a7b7281de
Krystian Gaj <krystian0120@outlook.com>
2024-03-05 18:12:35 +0100

    StatsViewModel refactoring; Update dependencies

--- app/src/main/java/com/example/techquiz/viewmodel/StatsViewModel.kt
+++ app/src/main/java/com/example/techquiz/viewmodel/StatsViewModel.kt
@@ -13,34 +13,47 @@ import kotlinx.coroutines.launch
 class StatsViewModel(
     private val statsRepository: StatsRepository,
 ) : ViewModel() {
-    private val _categoryStats = MutableStateFlow<List<CategoryStats>>(emptyList())
+    private val _categoryStats = MutableStateFlow(emptyList<CategoryStats>())
     val categoryStats get() = _categoryStats.asStateFlow()
 
-    private val _answeredQuestionsCount = MutableStateFlow(AnsweredQuestionsCountStats(0, 0))
+    private val _answeredQuestionsCount = MutableStateFlow(DEFAULT_COUNT_STATS)
     val answeredQuestionsCount get() = _answeredQuestionsCount.asStateFlow()
 
-    private val _answersCount = MutableStateFlow(CorrectAnswersStats(0, 0))
+    private val _answersCount = MutableStateFlow(DEFAULT_CORRECT_STATS)
     val correctAnswersCount get() = _answersCount.asStateFlow()
 
     fun getMostAnsweredCategories() {
         viewModelScope.launch {
-            _categoryStats.value = statsRepository.getMostAnsweredCategories(CATEGORIES_COUNT)
+            _categoryStats.value =
+                statsRepository.getMostAnsweredCategories(CATEGORIES_COUNT)
         }
     }
 
     fun getAnsweredQuestionsCount() {
         viewModelScope.launch {
-            _answeredQuestionsCount.value = statsRepository.getAnsweredQuestionsCount()
+            _answeredQuestionsCount.value =
+                statsRepository.getAnsweredQuestionsCount()
         }
     }
 
     fun getCorrectAnswersCount() {
         viewModelScope.launch {
-            _answersCount.value = statsRepository.getCorrectAnswersCount()
+            _answersCount.value =
+                statsRepository.getCorrectAnswersCount()
         }
     }
 
     private companion object {
         private const val CATEGORIES_COUNT = 3
+
+        private val DEFAULT_COUNT_STATS = AnsweredQuestionsCountStats(
+            questionsAnswered = 0,
+            allQuestions = 0,
+        )
+
+        private val DEFAULT_CORRECT_STATS = CorrectAnswersStats(
+            correctAnswers = 0,
+            allAnswers = 0,
+        )
     }
 }
