deef813aca9103c2a9c96da3ffef52518ea8d0b4
Krystian Gaj <krystian.gaj@student.uj.edu.pl>
2025-11-16 23:28:07 +0100

    Run StatsRepository's methods on IO dispatcher

--- app/src/main/java/com/example/techquiz/data/repository/StatsRepositoryDefault.kt
+++ app/src/main/java/com/example/techquiz/data/repository/StatsRepositoryDefault.kt
@@ -6,6 +6,8 @@ import com.example.techquiz.data.resources.Stats
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.resources.get
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
@@ -16,27 +18,31 @@ class StatsRepositoryDefault(
     override suspend fun getMostAnsweredCategories(
         userUuid: Uuid?,
         count: Int,
-    ): Result<List<CategoryStats>> = Result.runCatching {
-        val response = httpClient.get(
-            resource = Stats.MostAnsweredCategories(
-                userUuid = userUuid,
-                count = count,
-            ),
-        )
+    ): Result<List<CategoryStats>> = withContext(Dispatchers.IO) {
+        Result.runCatching {
+            val response = httpClient.get(
+                resource = Stats.MostAnsweredCategories(
+                    userUuid = userUuid,
+                    count = count,
+                ),
+            )
 
-        return@runCatching response.body()
+            return@runCatching response.body()
+        }
     }
 
     @OptIn(ExperimentalUuidApi::class)
     override suspend fun getCorrectAnswersCount(
         userUuid: Uuid?,
-    ): Result<CorrectAnswersStats> = Result.runCatching {
-        val response = httpClient.get(
-            resource = Stats.CorrectAnswersCount(
-                userUuid = userUuid,
-            ),
-        )
+    ): Result<CorrectAnswersStats> = withContext(Dispatchers.IO) {
+        Result.runCatching {
+            val response = httpClient.get(
+                resource = Stats.CorrectAnswersCount(
+                    userUuid = userUuid,
+                ),
+            )
 
-        return@runCatching response.body()
+            return@runCatching response.body()
+        }
     }
 }
