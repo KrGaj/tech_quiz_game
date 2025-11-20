67d730898b14925716e230d85a6c9d396650b2f4
Krystian Gaj <krystian.gaj@student.uj.edu.pl>
2025-11-20 02:33:46 +0100

    Remove obsolete Context.findActivity() extension method

--- app/src/main/java/com/example/techquiz/util/Extensions.kt
+++ app/src/main/java/com/example/techquiz/util/Extensions.kt
@@ -1,19 +1,6 @@
 package com.example.techquiz.util
 
-import android.app.Activity
-import android.content.Context
-import android.content.ContextWrapper
 import androidx.compose.runtime.MutableState
-
-// https://stackoverflow.com/a/74696154
-internal fun Context.findActivity(): Activity {
-    var context = this
-    while (context is ContextWrapper) {
-        if (context is Activity) return context
-        context = context.baseContext
-    }
-    throw IllegalStateException("Activity not found")
-}
 
 fun MutableState<Boolean>.toggleValue() {
     value = !value
