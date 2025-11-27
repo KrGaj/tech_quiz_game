ec5d02c08bb33e9f99db59cff491e47be62e8f0a
Krystian Gaj <krystian.gaj@student.uj.edu.pl>
2025-11-27 21:31:34 +0100

    Add unit tests for QuestionFlow

---
+++ app/src/test/java/com/example/techquiz/data/QuestionFlowTest.kt
@@ -0,0 +1,221 @@
+package com.example.techquiz.data
+
+import app.cash.turbine.test
+import com.example.techquiz.data.domain.Category
+import com.example.techquiz.data.domain.PossibleAnswer
+import com.example.techquiz.data.domain.Question
+import com.example.techquiz.data.repository.QuestionRepository
+import io.kotest.matchers.shouldBe
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+class QuestionFlowTest {
+    private lateinit var questionFlow: QuestionFlow
+    private lateinit var questionRepository: QuestionRepository
+
+    @Before
+    fun setUp() {
+        questionRepository = mockk()
+        questionFlow = QuestionFlow(questionRepository)
+    }
+
+    @Test
+    fun `After loading questions, first question is exposed`() = runTest {
+        coEvery {
+            questionRepository.getRandomQuestions(any(), any())
+        } returns Result.success(QUESTIONS)
+
+        questionFlow.loadQuestions(CATEGORY, QUESTIONS.size)
+
+        questionFlow.state.test {
+            with(awaitItem()) {
+                isCategoryEmpty shouldBe false
+                currentQuestion shouldBe QUESTIONS.first()
+                questionNumber shouldBe 1
+                hasNextQuestion shouldBe true
+                error shouldBe null
+            }
+        }
+    }
+
+    @Test
+    fun `After nextQuestion() call, next question is exposed correctly`() = runTest {
+        coEvery {
+            questionRepository.getRandomQuestions(any(), any())
+        } returns Result.success(QUESTIONS)
+
+        questionFlow.loadQuestions(CATEGORY, QUESTIONS.size)
+        questionFlow.nextQuestion()
+
+        questionFlow.state.test {
+            with(awaitItem()) {
+                isCategoryEmpty shouldBe false
+                currentQuestion shouldBe QUESTIONS[1]
+                questionNumber shouldBe 2
+                hasNextQuestion shouldBe true
+                error shouldBe null
+            }
+        }
+    }
+
+    @Test
+    fun `Last question is flagged correctly`() = runTest {
+        coEvery {
+            questionRepository.getRandomQuestions(any(), any())
+        } returns Result.success(QUESTIONS)
+
+        questionFlow.loadQuestions(CATEGORY, QUESTIONS.size)
+
+        (QUESTIONS.size - 1 downTo 1).forEach { _ ->
+            questionFlow.nextQuestion()
+        }
+
+        questionFlow.state.test {
+            with(awaitItem()) {
+                isCategoryEmpty shouldBe false
+                currentQuestion shouldBe QUESTIONS.last()
+                questionNumber shouldBe QUESTIONS.size
+                hasNextQuestion shouldBe false
+                error shouldBe null
+            }
+        }
+    }
+
+    @Test
+    fun `Lack of questions for given category is flagged correctly`() = runTest {
+        coEvery {
+            questionRepository.getRandomQuestions(any(), any())
+        } returns Result.success(emptyList())
+
+        questionFlow.loadQuestions(CATEGORY, QUESTIONS.size)
+
+        questionFlow.state.test {
+            awaitItem() shouldBe QuestionFlow.State(isCategoryEmpty = true)
+        }
+    }
+
+    @Test
+    fun `Error from repository is exposed in state`() = runTest {
+        val expectedException = IllegalStateException()
+
+        coEvery {
+            questionRepository.getRandomQuestions(any(), any())
+        } returns Result.failure(expectedException)
+
+        questionFlow.loadQuestions(CATEGORY, QUESTIONS.size)
+
+        questionFlow.state.test {
+            awaitItem() shouldBe QuestionFlow.State(error = expectedException)
+        }
+    }
+
+    @Test
+    fun `Nothing happens when trying to iterate outside question list`() = runTest {
+        coEvery {
+            questionRepository.getRandomQuestions(any(), any())
+        } returns Result.success(QUESTIONS)
+
+        questionFlow.loadQuestions(CATEGORY, QUESTIONS.size)
+
+        (QUESTIONS.size - 1 downTo 1).forEach { _ ->
+            questionFlow.nextQuestion()
+        }
+
+        questionFlow.nextQuestion()
+
+        questionFlow.state.test {
+            with(awaitItem()) {
+                isCategoryEmpty shouldBe false
+                currentQuestion shouldBe QUESTIONS.last()
+                questionNumber shouldBe QUESTIONS.size
+                hasNextQuestion shouldBe false
+                error shouldBe null
+            }
+        }
+    }
+
+    companion object {
+        private val CATEGORY = Category(name = "Demo Category")
+
+        private val QUESTIONS = listOf(
+            Question(
+                id = 1,
+                category = CATEGORY,
+                text = "Question ABC",
+                answers = listOf(
+                    PossibleAnswer(
+                        text = "Yes",
+                        isCorrect = true,
+                    ),
+                    PossibleAnswer(
+                        text = "No",
+                        isCorrect = false,
+                    ),
+                ),
+            ),
+            Question(
+                id = 2,
+                category = CATEGORY,
+                text = "Question xD",
+                answers = listOf(
+                    PossibleAnswer(
+                        text = "A",
+                        isCorrect = false,
+                    ),
+                    PossibleAnswer(
+                        text = "B",
+                        isCorrect = true,
+                    ),
+                ),
+            ),
+            Question(
+                id = 3,
+                category = CATEGORY,
+                text = "Example Question",
+                answers = listOf(
+                    PossibleAnswer(
+                        text = "1",
+                        isCorrect = true,
+                    ),
+                    PossibleAnswer(
+                        text = "2",
+                        isCorrect = false,
+                    ),
+                ),
+            ),
+            Question(
+                id = 5,
+                category = CATEGORY,
+                text = "Question DEF",
+                answers = listOf(
+                    PossibleAnswer(
+                        text = "Up",
+                        isCorrect = true,
+                    ),
+                    PossibleAnswer(
+                        text = "Down",
+                        isCorrect = false,
+                    ),
+                ),
+            ),
+            Question(
+                id = 8,
+                category = CATEGORY,
+                text = "Question MZ ETZ 251",
+                answers = listOf(
+                    PossibleAnswer(
+                        text = "Slow",
+                        isCorrect = false,
+                    ),
+                    PossibleAnswer(
+                        text = "Fast",
+                        isCorrect = true,
+                    ),
+                ),
+            ),
+        )
+    }
+}
